0d26c709c4edc4fefbe459cd89c28ec664879097
MUHAMMADARSALAN41002 <muhammadarsalantanoli1357@gmail.com>
2022-05-14 10:22:16 -0700

    last changes

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,13 +3,18 @@ import React from 'react'
 import { useNavigate } from 'react-router-dom';
 import Navbar from '../../components/navbar/Navbar';
 import './Home.css'
+import smit from '../../images/smit.png'
 const Home = () => {
     const navigate = useNavigate();
   return (
       <>
     <Navbar />
-
-    <Button type="primary" className='viewcourses-btn' onClick={() => navigate('/courses')}>View Courses</Button>
+    
+    <div style={{display:"flex", justifyContent:"center", alignItems:"center", width:"100%", height:"100vh"}}>
+    <h1 className='heading-first'>saylani mass it training program </h1>
+     <Button type="primary" className='viewcourses-btn' onClick={() => navigate('/courses')}>View Courses</Button>
+    </div>
+ 
       </>
   )
 }
